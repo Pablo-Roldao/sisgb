a5debc42598b6b1199ac489da1ed3eaba7985ca5
Pablo Santos <pablo.roldao.santos@gmail.com>
2023-02-20 10:52:56 -0300

    Configuring routes...

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import App from './App';
 import Home from './pages/Home';
+import About from './pages/About';
+import SignUp from './pages/SignUp';
+import BookCollection from './pages/BookCollection';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
@@ -13,6 +16,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route element={<App />}>
           <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/bookCollection' element={<BookCollection />} />
         </Route>
       </Routes>
     </BrowserRouter>
