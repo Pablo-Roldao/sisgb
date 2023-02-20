b167c8e3dc129eb15f1abf01aca4482d024b3781
Pablo Santos <pablo.roldao.santos@gmail.com>
2023-02-20 16:27:24 -0300

    Printing the values on the console...

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,48 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
 
 import NavbarComponent from '../components/NavbarComponent';
 import Footer from '../components/Footer';
-import { Button, Container, Form } from 'react-bootstrap';
 
-export default class SignUp extends Component {
-  render() {
-    return (
-      <>
-        <NavbarComponent about={true} bookCollection={true} />
-        
-        <div className='p-3'>
-          <Container className='border'>
-            <h3 className='text-center'>Cadastre-se</h3>
-            <Form action='sisgb-api.vercel.app/user/register'>
-              <Form.Group>
-                <Form.Label>Nome</Form.Label>
-                <Form.Control type='text' placeholder='Insira seu nome...' />
-              </Form.Group>
-              <Form.Group>
-                <Form.Label>CPF</Form.Label>
-                <Form.Control type='text' placeholder='Insira seu CPF...' />
-              </Form.Group>
-              <Form.Group>
-                <Form.Label>Data de nascimento</Form.Label>
-                <Form.Control type='date' placeholder='Insira sua data de nascimento...' />
-              </Form.Group>
-              <Form.Group>
-                <Form.Label>E-mail</Form.Label>
-                <Form.Control type='email' placeholder='Insira seu e-mail...' />
-              </Form.Group>
-              <Form.Group>
-                <Form.Label>Senha</Form.Label>
-                <Form.Control type='password' placeholder='Insira sua senha...' />
-              </Form.Group>
-              <div className='p-3 text-center'>
-                <Button type='submit'>Cadastrar</Button>
-              </div>
-            </Form>
-          </Container>
-        </div>
+export default function SignUp() {
 
-        <Footer />
-      </>
-    );
-  }
+  const { register, handleSubmit, formState: { erros } } = useForm();
+
+  const onSubmit = data => console.log(data);
+
+  return (
+    <>
+      <NavbarComponent about={true} bookCollection={true} />
+
+      <div className='p-3'>
+        <Container className='border'>
+          <h3 className='text-center'>Cadastre-se</h3>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <Form.Group>
+              <Form.Label>Nome</Form.Label>
+              <Form.Control type='text' name='name' {...register("name")} placeholder='Insira seu nome...' required />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>CPF</Form.Label>
+              <Form.Control type='text' name='cpf' {...register("cpf")} placeholder='Insira seu CPF...' required />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Data de nascimento</Form.Label>
+              <Form.Control type='date' name='birthDate' {...register("birthDate")} placeholder='Insira sua data de nascimento...' required />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>E-mail</Form.Label>
+              <Form.Control type='email' name='email' {...register("email")} placeholder='Insira seu e-mail...' required />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Senha</Form.Label>
+              <Form.Control type='password' name='password' {...register("password")} placeholder='Insira sua senha...' required />
+            </Form.Group>
+            <div className='p-3 text-center'>
+              <Button type='submit'>Cadastrar</Button>
+            </div>
+          </Form>
+        </Container>
+      </div>
+
+      <Footer />
+    </>
+  )
 }
