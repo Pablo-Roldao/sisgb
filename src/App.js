c5bac359af8dfc2f73bbf41df4850d2b38e0dedb
Pablo Santos <pablo.roldao.santos@gmail.com>
2023-02-20 10:37:41 -0300

    Adding routes...

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { Outlet } from "react-router-dom";
+
 function App() {
   return (
     <>
-      <h1>Welcome to SisGB App!</h1>
+      <div id='app-container'>
+        <Outlet />
+      </div>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import App from './App';
+import Home from './pages/Home';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path='/' element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
