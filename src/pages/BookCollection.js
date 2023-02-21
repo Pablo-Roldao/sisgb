c94267220f37c96b167c0344084c21a7e925366d
Pablo Santos <pablo.roldao.santos@gmail.com>
2023-02-21 14:11:53 -0300

    Getting books from API...

--- src/pages/BookCollection.js
+++ src/pages/BookCollection.js
@@ -1,10 +1,34 @@
-import React, { Component } from 'react';
+import React, {useEffect, useState } from 'react';
+import axios from 'axios';
 
-export default class BookCollection extends Component {
-  render() {
-    return (
-      <>
-      </>
-    );
+import NavbarComponent from '../components/NavbarComponent';
+
+export default function BookCollection() {
+
+  const [books, setBooks] = useState([]);
+
+  const getBooks = async () => {
+    try {
+      const response = await axios.get("https://sisgb-api.vercel.app/book/get-all");
+
+      const data = response.data;
+
+      console.log(data);
+      setBooks(data);
+    } catch (error) {
+      console.log(error);
+    }
   }
+
+  useEffect(() => {
+    getBooks()
+  }, []);
+
+  return (
+    <>
+
+      <NavbarComponent about={true} signUp={true} />
+
+    </>
+  );
 }
