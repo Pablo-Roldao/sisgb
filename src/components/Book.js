fd50495cf2db9cb3f09f7196dcc85ff503daea59
Pablo Santos <pablo.roldao.santos@gmail.com>
2023-02-21 15:17:18 -0300

    Starting book collection...

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { Container, Image } from 'react-bootstrap';
 
 export default function Book(props) {
+
+    let authors = JSON.stringify(props.authors);
+    authors = authors.replace("[\"", "");
+    authors = authors.replace("\"]", "");
+    authors = authors.replaceAll("\",\"", ", ");
+    
+
     return (
-        <Container>
+        <Container key={props.isbn}>
             <Image src={props.imgSrc} fluid className='rounded'/>
             <h4>{props.title}</h4>
-            <p>{props.authors}</p>
+            <p>{authors}</p>
         </Container>
     );
 }
--- src/pages/BookCollection.js
+++ src/pages/BookCollection.js
@@ -1,7 +1,10 @@
-import React, {useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+import { Col, Container, Row } from 'react-bootstrap';
+
 import NavbarComponent from '../components/NavbarComponent';
+import Book from '../components/Book';
 
 export default function BookCollection() {
 
@@ -24,10 +27,41 @@ export default function BookCollection() {
     getBooks()
   }, []);
 
+
+  let rowCounter = 0;
+  const booksResult = books.map((book) => {
+    if (rowCounter === 4) {
+      rowCounter = 1;
+      return (
+        <>
+          <Row></Row>
+          <Col>
+            <Book className='img-fluid' imgSrc='https://ihc.fcsh.unl.pt/wp-content/uploads/2018/01/livro001.jpg' title={book.title} authors={book.authors}></Book>
+          </Col>
+        </>
+      );
+    } else {
+      rowCounter++;
+      return (
+        <Col >
+          <Book className='img-fluid' imgSrc='https://ihc.fcsh.unl.pt/wp-content/uploads/2018/01/livro001.jpg' title={book.title} authors={book.authors}></Book>
+        </Col>
+      );
+    }
+  })
+
   return (
     <>
 
       <NavbarComponent about={true} signUp={true} />
+
+      <Container className='p-4'>
+        <h1 className='text-center'>Acervo</h1>
+        <br />
+        <Row>
+          {booksResult}
+        </Row>
+      </Container>
 
     </>
   );
