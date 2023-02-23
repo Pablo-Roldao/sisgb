2a81ce8210007662f5578161852d7ab6dcd8df44
Pablo Santos <pablo.roldao.santos@gmail.com>
2023-02-22 21:28:40 -0300

    "Finishing" Sign Up...

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import { useState } from 'react';
 import { Button, Container, Form } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 import axios from '../api/axios';
 
 import styles from './SignUp.module.css'
@@ -13,6 +14,8 @@ const REGISTER_URL = '/user/register';
 export default function SignUp() {
 
   const { register, handleSubmit } = useForm();
+
+  const [success, setSuccess] = useState(false);
 
   const onSubmit = async data => {
     const { name, cpf, birthDate, addres, email, password } = data;
@@ -33,7 +36,7 @@ export default function SignUp() {
         }
       );
       console.log(JSON.stringify(response));
-      
+      setSuccess(true);
     } catch (err) {
       if (!err?.response) {
         console.log("No Server Response!");
@@ -49,36 +52,45 @@ export default function SignUp() {
 
       <div className={styles.sign_up_form}>
         <Container className={styles.form}>
-          <h2 className='text-center'>Cadastre-se</h2>
-          <Form onSubmit={handleSubmit(onSubmit)}>
-            <Form.Group>
-              <Form.Label>Nome</Form.Label>
-              <Form.Control {...register("name")} type='text' name='name' placeholder='Insira seu nome...' required />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>CPF</Form.Label>
-              <Form.Control {...register("cpf")} type='text' name='cpf' placeholder='Insira seu CPF...' required />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Data de nascimento</Form.Label>
-              <Form.Control {...register("birthDate")} type='date' name='birthDate' placeholder='Insira sua data de nascimento...' required />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Endereço</Form.Label>
-              <Form.Control {...register("addres")} type='text' name='addres' placeholder='Insira seu endereço...' required />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>E-mail</Form.Label>
-              <Form.Control {...register("email")} type='email' name='email' placeholder='Insira seu e-mail...' required />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Senha</Form.Label>
-              <Form.Control {...register("password")} type='password' name='password' placeholder='Insira sua senha...' required />
-            </Form.Group>
-            <div className='p-3 text-center'>
-              <Button type='submit'>Cadastrar</Button>
-            </div>
-          </Form>
+          {success ? (
+            <>
+              <h2 className='text-center'>Usuário cadastrado com sucesso!</h2>
+              <h3 className=' text-center'>Seguir para <Link to='/' className={styles.login_button +' text-decoration-none'}>entrar</Link>...</h3>
+            </>
+          ) : (
+            <>
+              <h2 className='text-center'>Cadastre-se</h2>
+              <Form onSubmit={handleSubmit(onSubmit)}>
+                <Form.Group>
+                  <Form.Label>Nome</Form.Label>
+                  <Form.Control {...register("name")} type='text' name='name' placeholder='Insira seu nome...' required />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>CPF</Form.Label>
+                  <Form.Control {...register("cpf")} type='text' name='cpf' placeholder='Insira seu CPF...' required />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Data de nascimento</Form.Label>
+                  <Form.Control {...register("birthDate")} type='date' name='birthDate' placeholder='Insira sua data de nascimento...' required />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Endereço</Form.Label>
+                  <Form.Control {...register("addres")} type='text' name='addres' placeholder='Insira seu endereço...' required />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>E-mail</Form.Label>
+                  <Form.Control {...register("email")} type='email' name='email' placeholder='Insira seu e-mail...' required />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Senha</Form.Label>
+                  <Form.Control {...register("password")} type='password' name='password' placeholder='Insira sua senha...' required />
+                </Form.Group>
+                <div className='p-3 text-center'>
+                  <Button type='submit'>Cadastrar</Button>
+                </div>
+              </Form>
+            </>
+          )}
         </Container>
       </div>
 
