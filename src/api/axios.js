d3c889108cb8a169f27730e54ebffa9087930441
Pablo Santos <pablo.roldao.santos@gmail.com>
2023-02-26 20:43:19 -0300

    Chaging URL...

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-//const BASE_URL = 'https://sisgb-api.vercel.app/';
-const BASE_URL = 'http://localhost:9000/';
+const BASE_URL = 'https://sisgb-api.vercel.app/';
 
 export default axios.create({
     baseURL: BASE_URL
