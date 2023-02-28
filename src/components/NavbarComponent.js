50983a33e17ce90d1f23f21df1eea01af8d62152
Pablo Santos <pablo.roldao.santos@gmail.com>
2023-02-28 00:09:59 -0300

    Fixing navbar...

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -7,22 +7,22 @@ export default function NavbarComponent(props) {
 
     const nav = [];
 
-    if (props.about) { nav.push(<Nav.Link key={'about'}><Link to='/about'>Sobre nós</Link></Nav.Link>) };
-    if (props.signUp){ nav.push(<Nav.Link key={'SignUp'}><Link to='/signUp'>Cadastre-se</Link></Nav.Link>); }
+    if (props.about) { nav.push(<Nav.Item key={'about'}><Link to='/about'>Sobre nós</Link></Nav.Item>) };
+    if (props.signUp){ nav.push(<Nav.Item key={'SignUp'}><Link to='/signUp'>Cadastre-se</Link></Nav.Item>); }
 
-    if (props.adminDashboard){ nav.push(<Nav.Link key={'adminDashboard'}><Link to='/adminDashboard'>Dashboard</Link></Nav.Link>); }
-    if (props.dashboardFunctionary){ nav.push(<Nav.Link key={'dashboardFunctionary'}><Link to='/dashboardFunctionary'>Funcionários</Link></Nav.Link>); }
+    if (props.adminDashboard){ nav.push(<Nav.Item key={'adminDashboard'}><Link to='/adminDashboard'>Dashboard</Link></Nav.Item>); }
+    if (props.dashboardFunctionary){ nav.push(<Nav.Item key={'dashboardFunctionary'}><Link to='/dashboardFunctionary'>Funcionários</Link></Nav.Item>); }
 
-    if (props.functionaryDashboard){ nav.push(<Nav.Link key={'functionaryDashboard'}><Link to='/functionaryDashboard'>Dashboard</Link></Nav.Link>); }
-    if (props.dashboardBook)  { nav.push(<Nav.Link key={'dashboardBook'}><Link to='/dashboardBook'>Livros</Link></Nav.Link>); }
-    if (props.dashboardUser)  { nav.push(<Nav.Link key={'dashboardUser'}><Link to='/dashboardUser'>Usuários</Link></Nav.Link>); }
-    if (props.dashboardLoan) { nav.push(<Nav.Link key={'dashboardLoan'}><Link to='/dashboardLoan'>Empréstimos</Link></Nav.Link>); }
-    if (props.dashboardReservation) { nav.push(<Nav.Link key={'dashboardReservation'}><Link to='/dashboardReservation'>Reservas</Link></Nav.Link>); }
+    if (props.functionaryDashboard){ nav.push(<Nav.Item key={'functionaryDashboard'}><Link to='/functionaryDashboard'>Dashboard</Link></Nav.Item>); }
+    if (props.dashboardBook)  { nav.push(<Nav.Item key={'dashboardBook'}><Link to='/dashboardBook'>Livros</Link></Nav.Item>); }
+    if (props.dashboardUser)  { nav.push(<Nav.Item key={'dashboardUser'}><Link to='/dashboardUser'>Usuários</Link></Nav.Item>); }
+    if (props.dashboardLoan) { nav.push(<Nav.Item key={'dashboardLoan'}><Link to='/dashboardLoan'>Empréstimos</Link></Nav.Item>); }
+    if (props.dashboardReservation) { nav.push(<Nav.Item key={'dashboardReservation'}><Link to='/dashboardReservation'>Reservas</Link></Nav.Item>); }
 
-    if (props.bookCollectionUser)  { nav.push(<Nav.Link key={'bookCollectionUser'}><Link to='/bookCollectionUser'>Livros</Link></Nav.Link>); }
-    if (props.reservationsuser)  { nav.push(<Nav.Link key={'reservationsUser'}><Link to='/reservationsUser'>Reservas</Link></Nav.Link>); }
-    if (props.loansUser)  { nav.push(<Nav.Link key={'loansUser'}><Link to='/loansUser'>Empréstimos</Link></Nav.Link>); }
-    if (props.profile)  { nav.push(<Nav.Link key={'profile'}><Link to='/profile'>Perfil</Link></Nav.Link>); }
+    if (props.bookCollectionUser)  { nav.push(<Nav.Item key={'bookCollectionUser'}><Link to='/bookCollectionUser'>Livros</Link></Nav.Item>); }
+    if (props.reservationsuser)  { nav.push(<Nav.Item key={'reservationsUser'}><Link to='/reservationsUser'>Reservas</Link></Nav.Item>); }
+    if (props.loansUser)  { nav.push(<Nav.Item key={'loansUser'}><Link to='/loansUser'>Empréstimos</Link></Nav.Item>); }
+    if (props.profile)  { nav.push(<Nav.Item key={'profile'}><Link to='/profile'>Perfil</Link></Nav.Item>); }
 
 
     return (
@@ -37,7 +37,7 @@ export default function NavbarComponent(props) {
                         </Navbar.Brand>
                     </Col>
                     <Col sm={8}>
-                        <Nav className={styles.navbar_nav}>
+                        <Nav className={styles.navbar_nav} justify={true}>
                             {nav}
                         </Nav>
                     </Col>
