81aef13803fac930e65697cb891f37bef80847f7
Pablo Santos <pablo.roldao.santos@gmail.com>
2023-03-01 03:13:04 -0300

    Fixing mobile navbar...

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -9,24 +9,24 @@ export default function NavbarComponent(props) {
 
     const nav = [];
 
-    if (props.about) { nav.push(<Nav.Item key={'about'}><Link to='/about'>Sobre nós</Link></Nav.Item>) };
-    if (props.signUp) { nav.push(<Nav.Item key={'SignUp'}><Link to='/signUp'>Cadastre-se</Link></Nav.Item>); }
+    if (props.about) { nav.push(<Nav.Item key={'about'} className={styles.nav_item}><Link to='/about'>Sobre nós</Link></Nav.Item>) };
+    if (props.signUp) { nav.push(<Nav.Item key={'SignUp'} className={styles.nav_item}><Link to='/signUp'>Cadastre-se</Link></Nav.Item>); }
 
-    if (props.adminDashboard) { nav.push(<Nav.Item key={'adminDashboard'}><Link to='/adminDashboard'>Dashboard</Link></Nav.Item>); }
-    if (props.dashboardFunctionary) { nav.push(<Nav.Item key={'dashboardFunctionary'}><Link to='/dashboardFunctionary'>Funcionários</Link></Nav.Item>); }
+    if (props.adminDashboard) { nav.push(<Nav.Item key={'adminDashboard'} className={styles.nav_item}><Link to='/adminDashboard'>Dashboard</Link></Nav.Item>); }
+    if (props.dashboardFunctionary) { nav.push(<Nav.Item key={'dashboardFunctionary'} className={styles.nav_item}><Link to='/dashboardFunctionary'>Funcionários</Link></Nav.Item>); }
 
-    if (props.functionaryDashboard) { nav.push(<Nav.Item key={'functionaryDashboard'}><Link to='/functionaryDashboard'>Dashboard</Link></Nav.Item>); }
-    if (props.dashboardBook) { nav.push(<Nav.Item key={'dashboardBook'}><Link to='/dashboardBook'>Livros</Link></Nav.Item>); }
-    if (props.dashboardUser) { nav.push(<Nav.Item key={'dashboardUser'}><Link to='/dashboardUser'>Usuários</Link></Nav.Item>); }
-    if (props.dashboardLoan) { nav.push(<Nav.Item key={'dashboardLoan'}><Link to='/dashboardLoan'>Empréstimos</Link></Nav.Item>); }
-    if (props.dashboardReservation) { nav.push(<Nav.Item key={'dashboardReservation'}><Link to='/dashboardReservation'>Reservas</Link></Nav.Item>); }
+    if (props.functionaryDashboard) { nav.push(<Nav.Item key={'functionaryDashboard'} className={styles.nav_item}><Link to='/functionaryDashboard'>Dashboard</Link></Nav.Item>); }
+    if (props.dashboardBook) { nav.push(<Nav.Item key={'dashboardBook'} className={styles.nav_item}><Link to='/dashboardBook'>Livros</Link></Nav.Item>); }
+    if (props.dashboardUser) { nav.push(<Nav.Item key={'dashboardUser'} className={styles.nav_item}><Link to='/dashboardUser'>Usuários</Link></Nav.Item>); }
+    if (props.dashboardLoan) { nav.push(<Nav.Item key={'dashboardLoan'} className={styles.nav_item}><Link to='/dashboardLoan'>Empréstimos</Link></Nav.Item>); }
+    if (props.dashboardReservation) { nav.push(<Nav.Item key={'dashboardReservation'} className={styles.nav_item}><Link to='/dashboardReservation'>Reservas</Link></Nav.Item>); }
 
-    if (props.userDashboard) { nav.push(<Nav.Item key={'userDashboard'}><Link to='/userDashboard'>Dashboard</Link></Nav.Item>); }
-    if (props.bookCollectionUser) { nav.push(<Nav.Item key={'bookCollectionUser'}><Link to='/bookCollectionUser'>Livros</Link></Nav.Item>); }
-    if (props.reservationsUser) { nav.push(<Nav.Item key={'reservationsUser'}><Link to='/reservationsUser'>Reservas</Link></Nav.Item>); }
-    if (props.loansUser) { nav.push(<Nav.Item key={'loansUser'}><Link to='/loansUser'>Empréstimos</Link></Nav.Item>); }
-    if (props.profile) { nav.push(<Nav.Item key={'profile'}><Link to='/profile' state={{ userCpf: props.userCpf }}>{props.profile}</Link></Nav.Item>); }
-    if (!props.logoutHide) { nav.push(<Nav.Item key={'logout'}><Link to='/'>Sair</Link></Nav.Item>); }
+    if (props.userDashboard) { nav.push(<Nav.Item key={'userDashboard'} className={styles.nav_item}><Link to='/userDashboard'>Dashboard</Link></Nav.Item>); }
+    if (props.bookCollectionUser) { nav.push(<Nav.Item key={'bookCollectionUser'} className={styles.nav_item}><Link to='/bookCollectionUser'>Livros</Link></Nav.Item>); }
+    if (props.reservationsUser) { nav.push(<Nav.Item key={'reservationsUser'} className={styles.nav_item}><Link to='/reservationsUser'>Reservas</Link></Nav.Item>); }
+    if (props.loansUser) { nav.push(<Nav.Item key={'loansUser'} className={styles.nav_item}><Link to='/loansUser'>Empréstimos</Link></Nav.Item>); }
+    if (props.profile) { nav.push(<Nav.Item key={'profile'} className={styles.nav_item}><Link to='/profile' state={{ userCpf: props.userCpf }}>{props.profile}</Link></Nav.Item>); }
+    if (!props.logoutHide) { nav.push(<Nav.Item key={'logout'} className={styles.nav_item}><Link to='/'>Sair</Link></Nav.Item>); }
 
 
 
@@ -42,7 +42,7 @@ export default function NavbarComponent(props) {
                         </Navbar.Brand>
                     </Col>
                     <Col sm={8}>
-                        <Nav className={styles.navbar_nav} justify={true}>
+                        <Nav className={styles.navbar_nav}>
                             {nav}
                         </Nav>
                     </Col>
